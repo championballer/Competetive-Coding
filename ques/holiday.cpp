02a9b274a86b444d7f394566e0bb31951172b9ea
Shiv Kumar <championballer08@gmail.com>
2018-09-01 23:34:14 +0530

    improvement holseason

--- ques/holiday.cpp
+++ ques/holiday.cpp
@@ -9,28 +9,31 @@ int main()
     
     vector<char> arr(n);
     for(int i=0;i<n;i++)cin>>arr[i];
-    
     int count = 0;
     for(int i=0;i<=n-4;i++)
     {
         char current = arr[i];
-        int mark = 0;
+	int mark = 0;
         for(int j=i+2;j<n-1;j++)
         {
             if(arr[j]==current)
             {
                 mark = j;
+                cout<<i<<" "<<mark<<endl;
+                for(int k=i+1;k<mark;k++)
+        	{
+            		char tomatch = arr[k];
+            		for(int m=mark+1;m<n;m++)
+            		{
+                		if(arr[m]==tomatch)count++;
+            		}
+        	}
             }
+            
         }
-        if(mark==0)break;
-        for(int k=i+1;k<mark;k++)
-        {
-            char tomatch = arr[k];
-            for(int m=mark+1;m<n;m++)
-            {
-                if(arr[m]==tomatch)count++;
-            }
-        }
+        
+        if(mark==0)continue;
+        
         
     }
     
