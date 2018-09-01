f8559db7d15fa30e727ceb0e3c127a40a7f28805
Shiv Kumar <championballer08@gmail.com>
2018-09-01 23:03:58 +0530

    ls init

---
+++ ques/dp/wingame.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>>t;
+	
+	cout<<fixed<<showpoint;
+	while(t--)
+	{
+		int r,g;
+		cin>>r>>g;
+
+		double prob = 1.00000;		
+		if(r==0 || g==0)
+		{	
+			
+			cout<<setprecision(5);
+			cout<<prob<<endl;
+		}
+	}
+}
